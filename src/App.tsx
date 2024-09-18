da61bf1f97c48faec29328aaf4a90825a8a6eba4
yraj2001 <yaswamthrajvarikunta@gmail.com>
2024-09-18 17:24:32 -0500

    changed the ListGroup to Alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items1: string[] = [
-    "Chicago",
-    "Aurora",
-    "Naperville",
-    "Dekalb",
-    "Sycamore",
-  ];
-
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
 
   return (
     <>
-      <ListGroup
-        items={items1}
-        heading=" Cities"
-        onSelectedItem={handleSelectItem}
-      />
+      <Alert>
+        <span>Alert</span> Testing
+      </Alert>
     </>
   );
 }
