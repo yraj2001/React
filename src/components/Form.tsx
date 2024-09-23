1a41cd2d3f40f92cdce8cfb0a1beb61f595a8bea
yraj2001 <yaswamthrajvarikunta@gmail.com>
2024-09-23 14:34:12 -0500

    useForm is implemented

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,30 +1,53 @@
-import React, { FormEvent, useRef } from "react";
-import { useForm } from "react-hook-form";
+import React, { FormEvent, useRef, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+
+interface ForData {
+  name: string;
+  age: number;
+}
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = { name: "", age: 0 };
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    if (nameRef.current != null) person.name = nameRef.current.value;
-    if (ageRef.current != null) person.age = parseInt(ageRef.current.value);
-    console.log(person);
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ForData>();
+
+  const onSubmit = (data: FieldValues) => console.log(data);
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-input" />
+        <input
+          {...register("name", { required: true, minLength: 3 })}
+          id="name"
+          type="text"
+          className="form-input"
+        />
       </div>
+      {errors.name?.type === "required" && (
+        <p className="text-danger">The name field is required.</p>
+      )}
+      {errors.name?.type === "minLength" && (
+        <p className="text-danger">A name msut be atleast 3 characters</p>
+      )}
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} id="age" type="number" className="form-input" />
+        <input
+          {...register("age", { required: true })}
+          id="age"
+          type="number"
+          className="form-input"
+        />
       </div>
+      {errors.age?.type === "required" && (
+        <p className="text-danger">The age field is required.</p>
+      )}
       <button className="btn btn-primary" type="submit">
         Submit
       </button>
