4b6181f97527329bc0546c9595680b1f08d31ed9
yraj2001 <yaswamthrajvarikunta@gmail.com>
2024-09-28 04:08:29 -0500

    refresh

--- src/expense-tracker/categories.ts
+++ src/expense-tracker/categories.ts
@@ -1,3 +1,3 @@
 const categories = ["Entertainment", "Groceries", "Utilities"] as const;
-
+// only created as App.tsx is importing a file which uses categories. it creates a clash b/w both files
 export default categories;
